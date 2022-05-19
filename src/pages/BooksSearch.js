8edc5bc70b8872bfe2030723387f31c2b170b81a
Michal <noszczykmichal@wp.pl>
2022-05-19 23:53:17 +0200

    fetching the data from api

--- src/pages/BooksSearch.js
+++ src/pages/BooksSearch.js
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+
 function BooksSearch() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [loadedBooks, setLoadedBooks]=useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://gnikdroy.pythonanywhere.com/api/book/")
+      .then((response) => response.json())
+      .then((data) => {
+        setIsLoading(false);
+        let currentlyLoaded=data.results;
+        console.log(currentlyLoaded)
+        // setLoadedBooks(currentlyLoaded);
+      });
+  }, []);
+
+  let currentContent;
+
+  if (isLoading) {
+    currentContent = <div>It's loading</div>;
+  }
+
   return (
     <div>
       <h1>Search for a new book</h1>
+      {currentContent}
     </div>
   );
 }
