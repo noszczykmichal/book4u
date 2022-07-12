d28c5b9ef17efd9671b6d54efc333e8b0aa2b4d4
Michal <noszczykmichal@wp.pl>
2022-07-12 16:22:57 +0200

    shortened logic of the form validation

--- src/components/books/FindBookForm/FormDataAndValidation.js
+++ src/components/books/FindBookForm/FormDataAndValidation.js
@@ -19,50 +19,11 @@ export const languagesArray = [
 
 export const validationHandler = refObj => {
     let query = '';
-    const enteredTitle = refObj.title.current.value;
-    const enteredDescription = refObj.description.current.value;
-    const enteredAgentsType = refObj.agentsType.current.value;
-    const enteredAgentName = refObj.agentsName.current.value;
-    const enteredAgentsBirthDateMin = refObj.agentsBirthDateMin.current.value;
-    const enteredAgentsBirthDateMax = refObj.agentsBirthDateMax.current.value;
-    const enteredAgentsDeathDateMin = refObj.agentsDeathDateMin.current.value;
-    const enteredAgentsDeathDateMax = refObj.agentsDeathDateMax.current.value;
-    const enteredDownloadRangeMin = refObj.downloadRangeMin.current.value;
-    const enteredDownloadRangeMax = refObj.downloadRangeMax.current.value;
-    const enteredLanguage = refObj.language.current.value;
-
-    if (enteredTitle) {
-        query += `title_contains=${enteredTitle}&`;
-    }
-    if (enteredDescription) {
-        query += `description_contains=${enteredDescription}&`;
-    }
-    if (enteredAgentsType) {
-        query += `has_agent_type=${enteredAgentsType}&`;
-    }
-    if (enteredAgentName) {
-        query += `agent_name_contains=${enteredAgentName}&`;
-    }
-    if (enteredAgentsBirthDateMin) {
-        query += `agent_birth_date_range_min=${enteredAgentsBirthDateMin}&`;
-    }
-    if (enteredAgentsBirthDateMax) {
-        query += `agent_birth_date_range_max=${enteredAgentsBirthDateMax}&`;
-    }
-    if (enteredAgentsDeathDateMin) {
-        query += `agent_death_date_range_min=${enteredAgentsDeathDateMin}&`;
-    }
-    if (enteredAgentsDeathDateMax) {
-        query += `agent_death_date_range_max=${enteredAgentsDeathDateMax}&`;
-    }
-    if (enteredDownloadRangeMin) {
-        query += `downloads_range_min=${enteredDownloadRangeMin}&`;
-    }
-    if (enteredDownloadRangeMax) {
-        query += `downloads_range_max=${enteredDownloadRangeMax}&`;
-    }
-    if (enteredLanguage) {
-        query += `languages=${enteredLanguage}&`;
+    
+    for( let input in refObj){
+        if(refObj[input].current.value){
+            query+=`${refObj[input].current.name}=${refObj[input].current.value}&`
+        }
     }
 
     return query;
