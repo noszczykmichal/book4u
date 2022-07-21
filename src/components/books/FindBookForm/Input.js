56202a11c5b9000abc8c9f5ba1743026cfbc9233
Michal <noszczykmichal@wp.pl>
2022-07-21 23:48:56 +0200

    fixed code for other inputs plus styling

--- src/components/books/FindBookForm/Input.js
+++ src/components/books/FindBookForm/Input.js
@@ -4,11 +4,12 @@ import classes from './Input.module.css';
 import GlobalContext from '../../../store/global-context';
 
 function Input(props) {
-    let globalCtx = useContext(GlobalContext);
-    let inputValue=globalCtx.inputStoredValueObj[props.htmlName]
-    let currentValue=inputValue? inputValue : ''; 
+    const globalCtx = useContext(GlobalContext);
+    const inputValue = globalCtx.inputStoredValueObj[props.htmlName];
+    const currentValue = inputValue ? inputValue : '';
+    const [ classA, classB] = props.htmlClass;
 
-    return <input type={props.htmlType} id={props.htmlId} name={props.htmlName} className={classes[props.htmlClass]} value={currentValue} onChange={globalCtx.inputValueSetter} />
+    return <input type={props.htmlType} id={props.htmlId} name={props.htmlName} className={[classes[classA], classes[classB]].join(' ')} placeholder={props.htmlPlaceholder} value={currentValue} onChange={globalCtx.inputValueSetter} />
 }
 
 export default Input;
