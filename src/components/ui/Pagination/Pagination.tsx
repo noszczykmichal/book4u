62c028314bef58cc13652631aedb8044a03ffdec
Michal <noszczykmichal@wp.pl>
2025-01-17 15:32:20 +0100

    fixed the pagination input behaviour

--- src/components/ui/Pagination/Pagination.tsx
+++ src/components/ui/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useContext, KeyboardEvent } from "react";
+import { FormEvent, useContext, KeyboardEvent, useRef } from "react";
 
 import UIContext from "../../../store-context/uiContext";
 import classes from "./Pagination.module.css";
@@ -6,17 +6,22 @@ import PaginationArrow from "./PaginationArrow";
 
 const TablePagination = () => {
   const uiContext = useContext(UIContext);
+  const inputRef = useRef<HTMLInputElement>(null);
   const {
+    displayedPage,
     totalBooksAvail,
     setPaginationValue,
     displayedPageChangeHandler,
-    paginationValue,
+    // paginationValue,
   } = uiContext;
   const totalPagesCount = Math.ceil(totalBooksAvail / 10);
 
   const onKeyDownHandler = (event: KeyboardEvent) => {
-    if (event.key === "Enter") {
-      displayedPageChangeHandler(paginationValue);
+    const paginationInputValue = inputRef.current!.value.trim();
+    if (event.key === "Enter" && paginationInputValue !== "") {
+      displayedPageChangeHandler(+paginationInputValue);
+      inputRef.current!.value = "";
+      inputRef.current!.blur();
     }
   };
 
@@ -27,6 +32,15 @@ const TablePagination = () => {
     }
   };
 
+  const blurHandler = () => {
+    inputRef.current!.value = "";
+    inputRef.current!.setAttribute("placeholder", displayedPage.toString());
+  };
+  const focusHandler = () => {
+    inputRef.current!.value = displayedPage.toString();
+    inputRef.current!.setAttribute("placeholder", "");
+  };
+
   return (
     <div className={classes.pagination}>
       <PaginationArrow direction="previous" />
@@ -34,8 +48,11 @@ const TablePagination = () => {
         type="number"
         className={classes.pagination__input}
         onChange={paginationInputHandler}
-        value={paginationValue}
+        placeholder={displayedPage.toString()}
         onKeyDown={onKeyDownHandler}
+        onBlur={blurHandler}
+        onFocus={focusHandler}
+        ref={inputRef}
       />
       <span>of</span>
       <span>{totalPagesCount}</span>
